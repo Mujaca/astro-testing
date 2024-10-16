377d46da71e4508cfc4a7a797fd696ab4dc14266
Jean-Michel Reupsch <reupsch@brunsdigital.de>
2024-10-16 16:09:28 +0200

    cleaned test entry

--- src/content/config.ts
+++ src/content/config.ts
@@ -2,9 +2,6 @@ import { defineCollection, type CollectionConfig } from "astro:content";
 import { fileLoader } from "../fileLoader";
 
 const collections:any = {
-	"files": defineCollection({
-		loader: fileLoader({filepath: '/Users/jmreupsch/Documents/MD Vaults/Eminence of the Millenium/', pattern: '**/*.md'}),
-	})
 };
 
 export function addCollection(name:string, collection: CollectionConfig<any>) {
